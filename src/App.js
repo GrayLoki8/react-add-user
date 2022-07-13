0cb8585e24efa40d269e345ef33983d4af979760
Gray Loki <skuc090@gmail.com>
2022-07-13 17:25:47 +0300

    add component UserList

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import CreateUser from "./components/users/CreateUser";
+import UserList from "./components/users/UserList";
 
 function App() {
   return (
       <div>
         <CreateUser/>
+        <UserList users={[]}/>
       </div>
   )
 }
---
+++ src/components/users/UserList.js
@@ -0,0 +1,12 @@
+import Card from "../ui/Card";
+import styles from "./UserList.module.css"
+
+const UserList = (props) => {
+    return (<Card className={styles.users}>
+        <ul>
+            {props.users.map((user) => <li>{user.name} - {user.age} year</li>)
+            }
+        </ul>
+    </Card>)
+}
+export default UserList
