0652ab07e1176a0ee61c38342128be9c274dd709
Gray Loki <skuc090@gmail.com>
2022-07-13 16:56:24 +0300

    add validation and handler

--- src/components/users/CreateUser.js
+++ src/components/users/CreateUser.js
@@ -1,18 +1,35 @@
 import Card from "../ui/Card";
 import styles from './CreateUser.module.css'
 import Button from "../ui/Button";
+import {useState} from "react";
 const CreateUser=(props)=>{
+   const [inputName,setInputName]=useState("");
+    const [inputAge,setInputAge]= useState("");
   const createUserHandler=(event)=>{
       event.preventDefault();
+      if (inputName.trim().length===0 || inputAge.trim().length===0){
+          return;
+      }
+      if (+inputAge<1){
+          return;
+      }
+        setInputName("");
+        setInputAge("");
 
   }
+  const nameChangeHandler=(event)=>{
+      setInputName(event.target.value);
+  }
+    const ageChangeHandler=(event)=>{
+        setInputAge(event.target.value);
+    }
     return(
         <Card className={styles.input}>
         <form onSubmit={createUserHandler} >
             <label htmlFor="name">Name:</label>
-            <input id="name" type="text"/>
+            <input id="name" type="text" value={inputName}  onChange={nameChangeHandler}/>
             <label htmlFor="age">Age:</label>
-            <input id="age" type="number"/>
+            <input id="age" type="number" value={inputAge}  onChange={ageChangeHandler}/>
             <Button type="submit">Add user</Button>
         </form>
         </Card>
