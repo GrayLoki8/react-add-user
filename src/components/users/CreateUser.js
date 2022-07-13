787299c36c85256f60cc3a696aaa4e982156b050
Gray Loki <skuc090@gmail.com>
2022-07-13 16:29:58 +0300

    add component Card

--- src/components/users/CreateUser.js
+++ src/components/users/CreateUser.js
@@ -1,16 +1,20 @@
+import Card from "../ui/Card";
+import styles from './CreateUser.module.css'
 const CreateUser=(props)=>{
   const createUserHandler=(event)=>{
       event.preventDefault();
 
   }
     return(
-        <form onSubmit={createUserHandler}>
+        <Card className={styles.input}>
+        <form onSubmit={createUserHandler} >
             <label htmlFor="name">Name:</label>
             <input id="name" type="text"/>
             <label htmlFor="age">Age:</label>
             <input id="age" type="number"/>
             <button type="submit">Add user</button>
         </form>
+        </Card>
     )
 }
 export default CreateUser
